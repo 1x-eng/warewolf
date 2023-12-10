e02081231d4f6690733c3007fc58d1407fca2870
Pruthvi Kumar <pruthvikumar.123@gmail.com>
2023-12-10 17:30:44 +1100

    test: few more tests

--- tests/domain/robot/Robot.test.ts
+++ tests/domain/robot/Robot.test.ts
@@ -1,12 +1,18 @@
 import { Robot } from '../../../src/domain/robot/Robot';
 import { IRobotConfig } from '../../../src/domain/robot/IRobot';
+import logger from '../../../src/utils/logger';
 
 describe('Robot', () => {
   let robot: Robot;
   const mockConfig: IRobotConfig = { gridSizeX: 10, gridSizeY: 10 };
+  const spyLogger = jest.spyOn(logger, 'info');
 
   beforeEach(() => {
     robot = new Robot(mockConfig);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should move north correctly', () => {
@@ -36,6 +42,33 @@ describe('Robot', () => {
     robot['position'] = { x: 0, y: mockConfig.gridSizeY - 1 };
     robot.moveNorth();
     expect(robot.getPosition()).toEqual({ x: 0, y: mockConfig.gridSizeY - 1 });
+    expect(spyLogger).toHaveBeenCalledWith(
+      expect.stringContaining("Move ignored")
+    );
+  });
+
+  it('should not move south beyond grid boundary', () => {
+    robot['position'] = { x: 0, y: 0 };
+    robot.moveSouth();
+    expect(robot.getPosition()).toEqual({ x: 0, y: 0 });
+    expect(spyLogger).toHaveBeenCalledWith(expect.stringContaining("Move ignored"));
+  });
+
+  it('should not move north or east beyond grid boundaries at the northeast corner', () => {
+    robot['position'] = { x: mockConfig.gridSizeX - 1, y: mockConfig.gridSizeY - 1 };
+    robot.moveNorth();
+    robot.moveEast();
+    expect(robot.getPosition()).toEqual({ x: mockConfig.gridSizeX - 1, y: mockConfig.gridSizeY - 1 });
+    expect(spyLogger).toHaveBeenCalledTimes(2);
+    expect(spyLogger).toHaveBeenCalledWith(
+      expect.stringContaining("Move ignored")
+    );
+  });
+
+  it('should update its position correctly after a sequence of moves', () => {
+    robot.moveNorth();
+    robot.moveEast();  
+    expect(robot.getPosition()).toEqual({ x: 1, y: 1 });
   });
 
   it('should emit correct details in moved event on move north', () => {
@@ -46,6 +79,44 @@ describe('Robot', () => {
       direction: 'north',
       oldPosition: { x: 0, y: 0 },
       currentPosition: { x: 0, y: 1 },
+      gridSize: { x: mockConfig.gridSizeX, y: mockConfig.gridSizeY }
+    });
+  });
+
+  it('should emit correct details in moved event on move south', () => {
+    const spyEmit = jest.spyOn(robot, 'emit');
+    robot.moveNorth();
+    robot.moveSouth();
+    expect(spyEmit).toHaveBeenCalledWith('moved', {
+      type: 'moved',
+      direction: 'south',
+      oldPosition: { x: 0, y: 1 },
+      currentPosition: { x: 0, y: 0 },
+      gridSize: { x: mockConfig.gridSizeX, y: mockConfig.gridSizeY }
+    });
+  });
+
+  it('should emit correct details in moved event on move east', () => {
+    const spyEmit = jest.spyOn(robot, 'emit');
+    robot.moveEast();
+    expect(spyEmit).toHaveBeenCalledWith('moved', {
+      type: 'moved',
+      direction: 'east',
+      oldPosition: { x: 0, y: 0 },
+      currentPosition: { x: 1, y: 0 },
+      gridSize: { x: mockConfig.gridSizeX, y: mockConfig.gridSizeY }
+    });
+  });
+
+  it('should emit correct details in moved event on move west', () => {
+    const spyEmit = jest.spyOn(robot, 'emit');
+    robot.moveEast();
+    robot.moveWest();
+    expect(spyEmit).toHaveBeenCalledWith('moved', {
+      type: 'moved',
+      direction: 'west',
+      oldPosition: { x: 1, y: 0 },
+      currentPosition: { x: 0, y: 0 },
       gridSize: { x: mockConfig.gridSizeX, y: mockConfig.gridSizeY }
     });
   });
