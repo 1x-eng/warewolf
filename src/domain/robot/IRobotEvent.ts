d47cead32b7e672835ee4559825137aec4331451
Pruthvi Kumar <pruthvikumar.123@gmail.com>
2023-12-10 18:00:19 +1100

    refactor: cleanup

--- src/domain/robot/IRobotEvent.ts
+++ src/domain/robot/IRobotEvent.ts
@@ -9,11 +9,11 @@ interface IRobotEvent {
 }
 
 interface IRobotMovedEvent extends IRobotEvent {
-type: RobotEventType.Moved;
-direction: Direction;
-oldPosition: { x: number; y: number };
-currentPosition: { x: number; y: number };
-gridSize: { x: number; y: number };
+  type: RobotEventType.Moved;
+  direction: Direction;
+  oldPosition: { x: number; y: number };
+  currentPosition: { x: number; y: number };
+  gridSize: { x: number; y: number };
 }
 
 export type RobotEvent = IRobotMovedEvent;
